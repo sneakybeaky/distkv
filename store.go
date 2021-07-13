d022327e9df8760b33b0848134c3448a1c44639d
Jon Barber <jon.barber@acm.org>
2021-07-13 18:23:42 +0100

    Some comments for store

--- store.go
+++ store.go
@@ -8,11 +8,11 @@ import (
 )
 
 var (
-	enc = binary.BigEndian
+	enc = binary.BigEndian // encoding that we persist record sizes and index entries in
 )
 
 const (
-	lenWidth = 8
+	lenWidth = 8 // number of bytes used to store the records length
 )
 
 type store struct {
@@ -35,13 +35,17 @@ func newStore(f *os.File) (*store, error) {
 	}, nil
 }
 
+// Append persists the given bytes to the store and returns the number of bytes written and the position where the store holds the record in its file.
 func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	pos = s.size
+	// write the length of the record to the store so we know how many to read
 	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
 		return 0, 0, err
 	}
+
+	// write the record itself
 	w, err := s.buf.Write(p)
 	if err != nil {
 		return 0, 0, err
@@ -51,16 +55,21 @@ func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	return uint64(w), pos, nil
 }
 
+// Read returns the record at the given position
 func (s *store) Read(pos uint64) ([]byte, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	if err := s.buf.Flush(); err != nil {
 		return nil, err
 	}
+
+	// first read the size of the record
 	size := make([]byte, lenWidth)
 	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
 		return nil, err
 	}
+
+	// now read the record itself
 	b := make([]byte, enc.Uint64(size))
 	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
 		return nil, err
@@ -68,6 +77,7 @@ func (s *store) Read(pos uint64) ([]byte, error) {
 	return b, nil
 }
 
+// ReadAt fills p with bytes read beginning at the offset supplied
 func (s *store) ReadAt(p []byte, off int64) (int, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -77,6 +87,7 @@ func (s *store) ReadAt(p []byte, off int64) (int, error) {
 	return s.File.ReadAt(p, off)
 }
 
+// Close closes the store such that all data is persisted
 func (s *store) Close() error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
